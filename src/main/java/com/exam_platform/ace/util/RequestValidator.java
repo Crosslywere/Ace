855aef888e1d5baeab7a997a371cd86104b69493
Crosslywere <ogboruj@outlook.com>
2024-11-22 21:42:34 +0100

    fully implemented

--- src/main/java/com/exam_platform/ace/util/RequestValidator.java
+++ src/main/java/com/exam_platform/ace/util/RequestValidator.java
@@ -5,7 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 public class RequestValidator {
 
 	public static boolean isLocalhost(HttpServletRequest request) {
-		System.out.print("Request URL size = " + request.getRequestURL().length() + " Request URL = " + request.getRequestURL().toString());
-		return true;
+		String url = request.getRequestURL().substring(7);
+		return url.startsWith("localhost") || url.startsWith("127.0.0.1");
 	}
 }
