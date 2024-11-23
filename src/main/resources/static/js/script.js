3f4e1d84dadaf593c4f127e9b694b62beb7a4bab
Crosslywere <ogboruj@outlook.com>
2024-11-23 06:00:45 +0100

    prevented errors

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,4 +1,6 @@
 const toggler = document.querySelector(".toggler-btn");
-toggler.addEventListener("click", function(){
-    document.querySelector("#sidebar").classList.toggle("collapsed")
-});
+if (toggler != null) {
+    toggler.addEventListener("click", function(){
+        document.querySelector("#sidebar").classList.toggle("collapsed")
+    });
+}
