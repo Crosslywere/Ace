1cd41490025e96f3a48050b20824cd80f162b438
Crosslywere <ogboruj@outlook.com>
2024-11-23 05:47:25 +0100

    improved tests & model attributes

--- src/main/java/com/exam_platform/ace/controller/DashboardController.java
+++ src/main/java/com/exam_platform/ace/controller/DashboardController.java
@@ -1,5 +1,6 @@
 package com.exam_platform.ace.controller;
 
+import com.exam_platform.ace.entity.Candidate;
 import com.exam_platform.ace.entity.Exam;
 import com.exam_platform.ace.entity.Paper;
 import com.exam_platform.ace.entity.Question;
@@ -15,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -42,30 +44,73 @@ public class DashboardController {
 
 	private final ExamImporter examImporter;
 
+	private final int MAX_SIZE = 15;
+
 	@EventListener(ApplicationStartedEvent.class)
 	public void testRepository() {
-		Question question = Question.builder()
+		var questionBuilder = Question.builder()
 				.id(Question.Id.builder()
 						.number(1)
 						.build())
 				.query("\"What are you doing in my swap?\" What famous character said this line?")
 				.options(List.of("The Avatar", "Shrek", "Rick", "Ben 10", "Naruto"))
-				.answerIndex((byte)1)
-				.build();
-		Paper paper = Paper.builder()
-				.id(Paper.Id.builder()
+				.answerIndex((byte)1);
+		var paperBuilder = Paper.builder().id(Paper.Id.builder()
 						.name("Trivia")
-						.build())
-				.build();
-		paper.addQuestion(question);
-		Exam exam = Exam.builder()
-				.title("Trivia")
-				.scheduledDate(Date.valueOf(LocalDate.now()))
+						.build());
+		var examBuilder = Exam.builder()
 				.openTime(Time.valueOf(LocalTime.now()))
-				.closeTime(Time.valueOf(LocalTime.now().plusHours(1)))
-				.duration(10)
-				.passwordRequired(false)
+				.closeTime(Time.valueOf(LocalTime.now().plusHours(6)))
+				.duration(10);
+		var candidateBuilder = Candidate.builder();
+		for (int i = 0; i < 100; i++) {
+			Question q = questionBuilder.build();
+			Paper p = paperBuilder.build();
+			p.addQuestion(q);
+			Exam e = examBuilder
+					.title("Some Other Exam " + (i + 1))
+					.scheduledDate(Date.valueOf(LocalDate.now()))
+					.build();
+			e.addPaper(p);
+			for (int j = 0; j < 10; j++) {
+				Candidate c = candidateBuilder
+						.id(Candidate.Id.builder()
+								.username(String.valueOf(j))
+								.build())
+						.build();
+				e.addCandidate(c);
+			}
+			examService.createExam(e);
+		}
+		Question question1 = questionBuilder.build();
+		Paper paper1 = paperBuilder.build();
+		paper1.addQuestion(question1);
+		Exam exam1 = examBuilder
+				.title("Trivia 1(now)")
+				.scheduledDate(Date.valueOf(LocalDate.now()))
 				.build();
+		exam1.addPaper(paper1);
+		examService.createExam(exam1);
+
+		Question question2 = questionBuilder.build();
+		Paper paper2 = paperBuilder.build();
+		paper2.addQuestion(question2);
+		Exam exam2 = examBuilder
+				.title("Trivia 2(past)")
+				.scheduledDate(Date.valueOf(LocalDate.now().minusDays(1)))
+				.build();
+		exam2.addPaper(paper2);
+		examService.createExam(exam2);
+
+		Question question3 = questionBuilder.build();
+		Paper paper3 = paperBuilder.build();
+		paper3.addQuestion(question3);
+		Exam exam3 = examBuilder
+				.title("Trivia 3(tomorrow)")
+				.scheduledDate(Date.valueOf(LocalDate.now().plusDays(1)))
+				.build();
+		exam3.addPaper(paper3);
+		examService.createExam(exam3);
 	}
 
 	@GetMapping()
@@ -91,8 +136,8 @@ public class DashboardController {
 		var sorting = Sort.by("scheduledDate").ascending()
 				.and(Sort.by("openTime").ascending()
 						.and(Sort.by("title")));
-		var exams = examService.getExamsByState(Exam.State.SCHEDULED, PageRequest.of(pageNumber - 1, 10, sorting));
-		var maxPages = examService.getExamsByState(Exam.State.SCHEDULED).size() / 10;
+		var exams = examService.getExamsByState(Exam.State.SCHEDULED, PageRequest.of(pageNumber - 1, MAX_SIZE, sorting));
+		var maxPages = (int)examService.countExamsByState(Exam.State.SCHEDULED) / MAX_SIZE;
 		model.addAllAttributes(AttributeBuilder.buildForTable(
 				"Scheduled Exams", PageRoute.SCHEDULED,
 				exams, maxPages, pageNumber));
@@ -111,10 +156,10 @@ public class DashboardController {
 			return "redirect:/exam";
 		}
 		var sorting = Sort.by("closeTime").ascending();
-		var exams = examService.getExamsByState(Exam.State.ONGOING, PageRequest.of(pageNumber - 1, 10, sorting));
-		var maxPages = examService.getExamsByState(Exam.State.ONGOING).size() / 10;
+		var exams = examService.getExamsByState(Exam.State.ONGOING, PageRequest.of(pageNumber - 1, MAX_SIZE, sorting));
+		var maxPages = (int)examService.countExamsByState(Exam.State.ONGOING) / MAX_SIZE;
 		model.addAllAttributes(AttributeBuilder.buildForTable(
-				"Scheduled Exams", PageRoute.ONGOING,
+				"Ongoing Exams", PageRoute.ONGOING,
 				exams, maxPages, pageNumber));
 		return "examTable";
 	}
@@ -131,30 +176,54 @@ public class DashboardController {
 			return "redirect:/exam";
 		}
 		var sorting = Sort.by("closeTime").ascending();
-		var exams = examService.getExamsByState(Exam.State.RECORDED, PageRequest.of(pageNumber - 1, 10, sorting));
-		var maxPages = examService.getExamsByState(Exam.State.RECORDED).size() / 10;
+		var exams = examService.getExamsByState(Exam.State.RECORDED, PageRequest.of(pageNumber - 1, MAX_SIZE, sorting));
+		var maxPages = (int)examService.countExamsByState(Exam.State.RECORDED) / MAX_SIZE;
 		model.addAllAttributes(AttributeBuilder.buildForTable(
-				"Scheduled Exams", PageRoute.RECORDED,
+				"Recorded Exams", PageRoute.RECORDED,
 				exams, maxPages, pageNumber));
 		return "examTable";
 	}
 	//endregion
-	//region TODO Exam Search Table
+	//region Exam Search Table
+	@GetMapping("/search")
 	public String search(@RequestParam String search, Model model) {
-		if (search.isBlank()) {
+		if (search.isBlank() || search.length() < 4) {
 			return "redirect:/scheduled";
 		}
 		var results = examService.getExamsByTitle(search);
-		model.addAllAttributes(AttributeBuilder.buildForTable("Search Results", PageRoute.MIXTURE, results, 0, 0));
+		model.addAllAttributes(AttributeBuilder.buildForTable("Searched Exams Results", PageRoute.SEARCH, results, 0, 0));
 		return "examTable";
 	}
 	//endregion
 	//region TODO Candidate Management Table
 	@GetMapping("/manage/{id}")
-	public String candidates(@PathVariable("id") Long examId) {
-
-		return "redirect:/scheduled";
+	public String candidates(@PathVariable("id") Long examId, Model model, HttpServletRequest request) {
+		if (!RequestValidator.isLocalhost(request)) {
+			return "redirect:/exam";
+		}
+		Exam exam = examService.getExamById(examId);
+		if (exam != null && exam.getState() == Exam.State.ONGOING) {
+			List<Candidate> candidates = candidateService.getCandidatesByExam(exam, Pageable.unpaged());
+			model.addAllAttributes(AttributeBuilder.buildForTable("Manage " + exam.getTitle(), PageRoute.ONGOING, exam, candidates, 0, 0));
+			return "candidateTable";
+		}
+		return "redirect:/ongoing";
 	}
+	//region Candidates Search Table
+	@GetMapping("/manage/{id}/search")
+	public String candidates(@PathVariable("id") Long examId, @RequestParam String search, Model model, HttpServletRequest request) {
+		if (!RequestValidator.isLocalhost(request)) {
+			return "redirect:/exam";
+		}
+		Exam exam = examService.getExamById(examId);
+		if (exam != null && exam.getState() == Exam.State.ONGOING && !search.isBlank()) {
+			List<Candidate> candidates = candidateService.getCandidatesByExamAndUsername(exam, search);
+			model.addAllAttributes(AttributeBuilder.buildForTable("Search Results", PageRoute.SEARCH, exam, candidates, 0, 0));
+			return "candidateTable";
+		}
+		return "redirect:/ongoing";
+	}
+	//endregion
 	//endregion
 //endregion
 
@@ -168,9 +237,6 @@ public class DashboardController {
 		if (session.getAttribute("exam") == null) {
 			session.setAttribute("exam", new Exam());
 		}
-		model.addAttribute("examType", Exam.State.SCHEDULED.name());
-		model.addAttribute("currentRoute", PageRoute.CREATE.name());
-		model.addAttribute("pageTitle", "Create Exam");
 		model.addAllAttributes(AttributeBuilder.build("Create Exam", PageRoute.CREATE));
 		return "examForm";
 	}
@@ -190,42 +256,38 @@ public class DashboardController {
 		return create(model, session, request);
 	}
 
-	@PostMapping("/created")
+	@PostMapping("/create-f")
 	public String create(HttpSession session, RedirectAttributes attributes) {
 		Exam exam = (Exam)session.getAttribute("exam");
 		session.removeAttribute("exam");
 		exam = examService.createExam(exam);
-		attributes.addFlashAttribute("notification",new Notification("success", exam, 0L, "Created exam successfully"));
-		switch (exam.getState()) {
-			case SCHEDULED -> {
-				return "redirect:/scheduled";
-			}
-			case ONGOING -> {
-				return "redirect:/ongoing";
-			}
-			case RECORDED -> {
-				return "redirect:/recorded";
-			}
-			default -> {
-				return "redirect:/";
-			}
-		}
+		attributes.addFlashAttribute("notification", new Notification("success", exam, 0L, "Created exam successfully"));
+		return "redirect:/scheduled";
 	}
 	//endregion
-	//region TODO Modifying an exam
-	@GetMapping("/modify/{id}")
+	//region TODO Updating an exam
+	@GetMapping("/update/{id}")
 	public String modify(@PathVariable("id") Long examId, HttpSession session, HttpServletRequest request) {
 		if (!RequestValidator.isLocalhost(request)) {
 			return "redirect:/exam";
 		}
 		Exam exam = examService.getExamById(examId);
-		if (exam == null) {
-			return "redirect:/";
+		if (exam == null || exam.getState() != Exam.State.SCHEDULED) {
+			return "redirect:/recorded";
 		}
-		return"";
+		throw new UnsupportedOperationException("This route has not been built yet");
 	}
 	//endregion
 	//region TODO Exporting an exam
+	//endregion
+//endregion
+
+//region Functionalities
+	//region Delete Exam
+
+	//endregion
+	//region Stop Exam
+
 	//endregion
 //endregion
 }
