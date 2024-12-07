f9a7e2cecc22e844b8387a3b2f585fba430ff38d
Crosslywere <ogboruj@outlook.com>
2024-12-07 19:42:02 +0100

    added script to refresh on navigation

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -11,3 +11,10 @@ function timeWriter(t) {
     var s = Math.floor(t % 60);
     return (h > 0 ? h + ':' : '') + (m >= 10 ? m : '0' + m) + ':' + (s >= 10 ? s : '0' + s);
 }
+
+window.addEventListener("pageshow", event => {
+    var historyTraversal = event.persisted || (typeof window.performance != "undefined" && window.performance.navigation.type === 2);
+    if (historyTraversal) {
+        window.location.reload();
+    }
+});
